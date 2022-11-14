1368db5d0104484d817d634889b03bc6f2ec8261
brandreplicas <brandreplicastore@gmail.com>
2022-11-14 01:43:40 +0000

    just backup

--- upload.js
+++ upload.js
@@ -2,6 +2,31 @@
 
 (function(document, window, console){
   console.log('script started');
+
+  let put_file = function(out, attachments, i) {
+    if(!attachments.hasOwnProperty(i)){
+      out.innerHTML += '<br/>You may upload now next stock!';
+      return;
+    }
+    let f = attachments[i];
+    out.innerHTML += '<br/>Uploading: ' + f.name;
+    const formData = new FormData();
+    formData.append('stock', f);
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+      console.log(xhr.responseText);
+      out.innerHTML += '<br/>' + xhr.responseText;
+      put_file(out, attachments, i+1);
+    }
+    xhr.onerror = () => {
+      console.log(xhr.statusText);
+      out.innerHTML += '<br/>Something went wrong!';
+      put_file(out, attachments, i+1);
+    }
+    xhr.open('POST', 'upload.php', true);
+    xhr.send(formData);
+  };
+
   document.addEventListener('DOMContentLoaded', function(e){
     let out = document.querySelector('#out');
     let files = document.querySelector('#files');
@@ -12,26 +37,7 @@
         return;
       }
       out.textContent = 'Please wait!';
-      console.log(attachments);
-      for(var i in attachments){
-        if(!attachments.hasOwnProperty(i))continue;
-        let f = attachments[i];
-        out.innerHTML += '<br/>Uploading: ' + f.name;
-        const formData = new FormData();
-        formData.append('stock', f);
-        const xhr = new XMLHttpRequest();
-        xhr.onload = () => {
-          console.log(xhr.responseText);
-          out.innerHTML += '<br/>' + xhr.responseText;
-        }
-        xhr.onerror = () => {
-          console.log(xhr.statusText);
-          out.innerHTML += '<br/>Something went wrong!';
-        }
-        xhr.open('POST', 'upload.php', false);
-        xhr.send(formData);
-      }
-      out.innerHTML += '<br/>You may upload now next stock!';
+      put_file(out, attachments, 0);
     }, false);
   }, false);
 })(document, window, console);
